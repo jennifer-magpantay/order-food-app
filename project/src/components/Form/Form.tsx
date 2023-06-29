1cc4bfb4b4a1da20de2bdadcb0e6cab5c2f453f2
Jennifer Magpantay <56962997+jennifer-magpantay@users.noreply.github.com>
2023-06-29 16:59:01 +0200

    Update Form.tsx

--- project/src/components/Form/Form.tsx
+++ project/src/components/Form/Form.tsx
@@ -1,34 +1,32 @@
 import React, { useContext } from "react";
 import { OrdersContext } from "../../store/ordersContext";
 import { Button } from "../UI/Button";
+import { formatPrice } from "../../helpers/randomPrice";
 
 interface FormProps {
   dataId: string;
   children: React.ReactNode;
 }
 
-//     const handleSubmitItem = (e: React.FormEvent<HTMLFormElement>) => {
-//       e.preventDefault();
-//       const target = e.currentTarget as HTMLFormElement;
-//       onSubmitItem(target.id);
-//     };
-
-//     return (
-//       <form onSubmit={handleSubmitItem} id={id} ref={ref}>
-//         <InputCounter itemAmount={} itemId={id} />
-//         <Button id="submit" type="submit" text="Add" customClasses="cta add" />
-//       </form>
-//     );
-//   }
-// );
-
 export const Form = ({ dataId, children }: FormProps) => {
   const { menu, addItem } = useContext(OrdersContext);
+
+  const calculateSubtotal = (amount: number, price: string) => {
+    const _price = price.replace("€", "").replace(",", ".").trim();
+    const subtotal = amount * Number(_price);
+    return formatPrice(subtotal);
+  };
   const handleSubmitItem = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const target = e.currentTarget as HTMLFormElement;
     const filteredItem = menu?.find((item) => item.id === target.dataset.id);
-    filteredItem && addItem(filteredItem);
+    if (filteredItem) {
+      const newItem = {
+        ...filteredItem,
+        subtotal: calculateSubtotal(filteredItem?.amount, filteredItem.price),
+      };
+      addItem(newItem);
+    }
   };
 
   return (
